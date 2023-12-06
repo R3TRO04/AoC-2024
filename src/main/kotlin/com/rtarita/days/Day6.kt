c92f41aeb468926edc91c599792421ecd1abd027
Raphael Tarita <raphael.tarita@gmail.com>
2023-12-06 12:58:30 +0100

    [Day 6] solution

--- src/main/kotlin/com/rtarita/days/Day6.kt
+++ src/main/kotlin/com/rtarita/days/Day6.kt
@@ -2,16 +2,64 @@ package com.rtarita.days
 
 import com.rtarita.structure.AoCDay
 import com.rtarita.util.day
+import com.rtarita.util.sqr
 import kotlinx.datetime.LocalDate
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
 object Day6 : AoCDay {
+    private val SPACE_REGEX = Regex("\\s+")
     override val day: LocalDate = day(6)
 
-    override fun executePart1(input: String): Any {
-        return "(not implemented)"
+    private fun parseInput(input: String): Map<Long, Long> {
+        val (time, distance) = input.lines()
+            .map { line ->
+                line.trimStart { !it.isDigit() && !it.isWhitespace() }
+                    .trim()
+                    .split(SPACE_REGEX)
+                    .map { it.toLong() }
+            }
+
+        return time.zip(distance)
+            .toMap()
     }
 
+    private fun solveQuadratic(a: Long, b: Long, c: Long): Pair<Double, Double>? {
+        val discriminant = sqr(b) - 4 * a * c
+        return if (discriminant < 0) {
+            null
+        } else {
+            val root = sqrt(discriminant.toDouble())
+            ((-b + root) / 2 * a) to ((-b - root) / 2 * a)
+        }
+    }
+
+    private fun rangeSize(lower: Double, upper: Double) = (ceil(upper).roundToInt() - 1) - (floor(lower).roundToInt() + 1) + 1
+
+    /**
+     * The distances resulting in holding down the button for n milliseconds follow a parabola defined as
+     * `-square(n) + time * n`. The current record distance in this mathematical representation is a
+     * line that is parallel to the n-Axis. The equation `-square(n) + time * n == recordDist` yields
+     * two solutions, which are precisely the lower and upper bound of the possible solutions. The equation
+     * can be solved using the quadratic formula.
+     */
+    override fun executePart1(input: String) = parseInput(input).entries
+        .map { (time, recordDist) ->
+            val (lower, upper) = solveQuadratic(-1, time, -recordDist) ?: error("unsolvable")
+            rangeSize(lower, upper)
+        }
+        .reduce { a, b -> a * b }
+
     override fun executePart2(input: String): Any {
-        return "(not implemented)"
+        val (time, recordDist) = input.lines()
+            .map { line ->
+                line.filter { it.isDigit() }
+                    .toLong()
+            }
+
+        val (lower, upper) = solveQuadratic(-1, time, -recordDist) ?: error("unsolvable")
+        return rangeSize(lower, upper)
     }
 }
