8a2e38cf0621915419560f6b71222d2cb06e2fad
Nico Thomas Beranek <nico@jube.at>
2024-12-04 16:24:06 +0100

    big uff 😩

--- src/main/kotlin/com/r3tro04/days/Day4.kt
+++ src/main/kotlin/com/r3tro04/days/Day4.kt
@@ -8,15 +8,21 @@ object Day4 : AoCDay {
     override val day: LocalDate = day(4)
     private val searchChars = "XMAS"
 
+    // should be 2554
     override fun executePart1(input: String): Any {
-        val length = input.lines().first().length
-        val depth = input.lines().size
-        val charArray = Array(length) {
-            CharArray(depth)
+        val length = input.lines().first().lastIndex
+        val depth = input.lines().lastIndex
+        val charArray = Array(length + 1) {
+            CharArray(depth + 1)
+        }
+        input.lineSequence().forEachIndexed { indexLine, s ->
+            s.forEachIndexed { indexChar, c ->
+                charArray[indexChar][indexLine] = c
+            }
         }
         val indices = buildMap {
             put(DirectionStart.LEFT, buildList {
-                for (i in 0 until length) {
+                for (i in 0..length) {
                     if (i + 3 <= length) {
                         add(
                             listOf(
@@ -29,8 +35,8 @@ object Day4 : AoCDay {
                     }
                 }
             })
-            put( DirectionStart.RIGHT, buildList {
-                for (i in 0 until length) {
+            put(DirectionStart.RIGHT, buildList {
+                for (i in 0..length) {
                     if (i + 3 <= length) {
                         add(
                             listOf(
@@ -44,7 +50,7 @@ object Day4 : AoCDay {
                 }
             })
             put(DirectionStart.UP, buildList {
-                for (i in 0 until depth) {
+                for (i in 0..depth) {
                     if (i + 3 <= depth) {
                         add(
                             listOf(
@@ -58,7 +64,7 @@ object Day4 : AoCDay {
                 }
             })
             put(DirectionStart.DOWN, buildList {
-                for (i in 0 until depth) {
+                for (i in 0..depth) {
                     if (i + 3 <= depth) {
                         add(
                             listOf(
@@ -74,42 +80,93 @@ object Day4 : AoCDay {
         }
 
         return buildList {
-            for (i in 0 until length) {
+            for (i in 0..depth) {
                add(
                    indices.filter {
                         it.key == DirectionStart.LEFT || it.key == DirectionStart.RIGHT
                     }.map {
                         it.value
                     }.map {
-                        it.flatten().map { index ->
-                            if (index == 140) charArray[index - 1][i]
-                            else charArray[index][i]
+                        it.map { innerList ->
+                            innerList.map { index ->
+                                charArray[index][i]
+                            }
                         }
-                    }.count {
-                        it.toString() == searchChars
+                    }.sumOf {
+                        it.count { chars ->
+                            val s = chars[0].toString() + chars[1] + chars[2] + chars[3]
+                            s == searchChars
+                        }
                     }
                )
             }
-            for (i in 0 until depth) {
+            for (i in 0..length) {
                 add(
                     indices.filter {
                         it.key == DirectionStart.UP || it.key == DirectionStart.DOWN
                     }.map {
                         it.value
                     }.map {
-                        it.flatten().map { index ->
-                            if (index == 140) charArray[i][index - 1]
-                            else charArray[i][index]
-
+                        it.map { innerList ->
+                            innerList.map { index ->
+                                charArray[i][index]
+                            }
                         }
-                    }.count {
-                        it.toString() == searchChars
+                    }.sumOf {
+                        it.count { chars ->
+                            val s = chars[0].toString() + chars[1] + chars[2] + chars[3]
+                            s == searchChars
+                        }
                     }
                 )
+            }
+            for (i in 0..length) {
+                for (j in 0..depth) {
+                    add(
+                        buildList {
+                            add(
+                                buildList {
+                                    for (d in 0..3) {
+                                        add(charArray.diagonalRightUp(i, j, d))
+                                    }
+                                }
+                            )
+                            add(
+                                buildList {
+                                    for (d in 0..3) {
+                                        add(charArray.diagonalRightDown(i, j, d))
+                                    }
+                                }
+                            )
+                            add(
+                                buildList {
+                                    for (d in 0..3) {
+                                        add(charArray.diagonalLeftUp(i, j, d))
+                                    }
+                                }
+                            )
+                            add(
+                                buildList {
+                                    for (d in 0..3) {
+                                        add(charArray.diagonalLeftDown(i, j, d))
+                                    }
+                                }
+                            )
+                        }.map {
+                            it.filterNotNull()
+                        }.filter {
+                            it.size == 4
+                        }.count { chars ->
+                            val s = chars[0].toString() + chars[1] + chars[2] + chars[3]
+                            s == searchChars
+                        }
+                    )
+                }
             }
         }.sum()
     }
 
+    // should be 1916
     override fun executePart2(input: String): Any {
         return "(not implemented)"
     }
@@ -119,5 +176,42 @@ object Day4 : AoCDay {
         RIGHT,
         UP,
         DOWN,
+    }
+
+    private fun Array<CharArray>.diagonalRightDown(i: Int, j: Int, index: Int) : Char? {
+        var res : Char? = null
+        runCatching {
+            this[i+1+index][j+1+index]
+        }.onSuccess {
+            res = it
+        }
+        return res
+    }
+    private fun Array<CharArray>.diagonalRightUp(i: Int, j: Int, index: Int) : Char? {
+        var res : Char? = null
+        runCatching {
+            this[i+1+index][j-1-index]
+        }.onSuccess {
+            res = it
+        }
+        return res
+    }
+    private fun Array<CharArray>.diagonalLeftDown(i: Int, j: Int, index: Int) : Char? {
+        var res : Char? = null
+        runCatching {
+            this[i-1-index][j+1+index]
+        }.onSuccess {
+            res = it
+        }
+        return res
+    }
+    private fun Array<CharArray>.diagonalLeftUp(i: Int, j: Int, index: Int) : Char? {
+        var res : Char? = null
+        runCatching {
+            this[i-1-index][j-1-index]
+        }.onSuccess {
+            res = it
+        }
+        return res
     }
 }
